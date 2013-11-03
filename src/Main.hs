8c9fcea1e17c2d40d2bd8cc672f4aa3fd975435f
osa1 <omeragacan@gmail.com>
2013-11-03 20:31:27 +0200

    improvements in UI

--- src/Main.hs
+++ src/Main.hs
@@ -41,39 +41,43 @@ run opts@KTestOptions{..} = do
       else runKRuns verbose threads timeout (fromMaybe tests ss)
     return ()
 
+-- TODO: This function assumes every definition has at most one test case,
+-- is it really the case?
 runKompiles :: Bool -> Int -> [TestCase] -> K [TestCase]
 runKompiles verbose _ tests = do
     --when verbose $ liftIO . putStrLn $ compiling
-    results <- liftIO $ parallel $ map mkKompileProc tests
-    liftIO $ print (map fst results)
+    liftIO . putStrLn $ concat [ "Kompiling ", show (length tests), " definitions." ]
+    results <- liftIO $ parallel $ zipWith (mkKompileProc (length tests)) [1 ..] tests
     return (map snd results)
   where
-    mkKompileProc :: TestCase -> IO (Bool, TestCase)
-    mkKompileProc tc = do
+    mkKompileProc :: Int ->  Int -> TestCase -> IO (Bool, TestCase)
+    mkKompileProc totalTestN pnum tc = do
       let args = [definition tc]
-      putStrLn $ "creating kompile process with args: " ++ show args
+      printInfoMsg "kompile" args pnum totalTestN
       (_, _, Just stderr, phandle) <- createProcess (proc "kompile" args){std_err=CreatePipe}
       exitCode <- waitForProcess phandle
       case exitCode of
         ExitFailure _ -> do
           errMsg <- hGetContents stderr
-          putStrLn $ concat [ "kompile failed: ", errMsg, "\nargs were: ", show args ]
+          putStrLn $ concat [ "FAILURE: program ", show pnum, " failed with error:\n    ", errMsg ]
           return (False, tc)
         ExitSuccess -> return (True, tc)
 
 runPdfs :: Bool -> Int -> [TestCase] -> K ()
-runPdfs verbose _ tests = liftIO $ parallel_ $ map mkPdfProc tests
+runPdfs verbose _ tests = liftIO $ do
+    putStrLn $ concat [ "Generating PDFs for ", show (length tests), " definitions." ]
+    parallel_ $ zipWith (mkPdfProc (length tests)) [1 ..] tests
   where
-    mkPdfProc :: TestCase -> IO Bool
-    mkPdfProc tc = do
+    mkPdfProc :: Int -> Int -> TestCase -> IO Bool
+    mkPdfProc totalTestN pnum tc = do
       let args = [definition tc, "--backend=pdf"]
-      putStrLn $ "creating kompile process with args: " ++ show args
+      printInfoMsg "kompile" args pnum totalTestN
       (_, _, Just stderr, phandle) <- createProcess (proc "kompile" args){std_err=CreatePipe}
       exitCode <- waitForProcess phandle
       case exitCode of
         ExitFailure _ -> do
           errMsg <- hGetContents stderr
-          putStrLn $ concat [ "kompile failed: ", errMsg, "\nargs were: ", show args ]
+          putStrLn $ concat [ "FAILURE: program ", show pnum, " failed with error:\n    ", errMsg ]
           return False
         ExitSuccess -> return True
 
@@ -97,7 +101,7 @@ runKRuns verbose _ timeout tests = do
     mkKrunProc :: Int -> Int -> (FilePath, FilePath, Maybe FilePath, Maybe FilePath, Maybe FilePath) -> IO Bool
     mkKrunProc totalTestN pnum (defPath, prog, stdinf, stdoutf, stderrf) = do
       let args = [prog, "--directory=" ++ takeDirectory defPath]
-      putStrLn $ concat [ "krun ", unwords args, " [", show pnum, " / ", show totalTestN, "]" ]
+      printInfoMsg "krun" args pnum totalTestN
       (Just stdin, Just stdout, Just stderr, phandle) <-
         createProcess (proc "krun" args){std_in=CreatePipe, std_out=CreatePipe, std_err=CreatePipe}
       maybe (return ()) (\f -> hPutStr stdin =<< readFile f) stdinf
@@ -160,6 +164,10 @@ runKRuns verbose _ timeout tests = do
     countJusts :: (a -> Maybe b) -> [a] -> Int
     countJusts fn []       = 0
     countJusts fn (x : xs) = maybe 0 (const 1) (fn x) + countJusts fn xs
+
+printInfoMsg :: String -> [String] -> Int -> Int -> IO ()
+printInfoMsg pname args pnum total =
+    putStrLn $ concat [ pname, " ", unwords args, " [", show pnum, " / ", show total, "]" ]
 
 -- TODO: show help message when it's run without arguments
 main :: IO ()
